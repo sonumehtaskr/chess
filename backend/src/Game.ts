46ac0dabee3ee9d10e42ed795599919c1b0a6b42
sonumehtaskr <sonumehtaskr@gmail.com>
2025-07-07 22:43:03 +0530

    first commit

---
+++ backend/src/Game.ts
@@ -0,0 +1,27 @@
+import { WebSocket, WebSocketEventMap } from "ws";
+
+export class Game {
+    public player1: WebSocket;
+    public player2: WebSocket;
+    private board: string;
+    private moves: string[];
+    private startTime: Date;
+
+    constructor(player1: WebSocket, player2: WebSocket) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = "";
+        this.moves = [];
+        this.startTime = new Date();
+    }
+
+    makeMove(socket: WebSocket,move:string){
+        // validation here
+        //is it this users move
+        //is the move valid
+        //update the board
+        //push the move
+        //check if the game is over
+        //send the update board to both players
+    }
+}
